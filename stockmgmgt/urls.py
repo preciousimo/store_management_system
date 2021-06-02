898ac9dae2ab4192f8fe9e555d5c31cde79f12d0
preciousimo <preciousimoniakemu@gmail.com>
2021-06-02 16:55:32 -0700

    List items from database

--- stockmgmgt/urls.py
+++ stockmgmgt/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name='home'),
+    path('list_item/', views.list_item, name='list_item'),
 ]
--- stockmgmgt/views.py
+++ stockmgmgt/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
 	title = 'Welcome: This is the Home Page'
+    form  = 'Form for home page'
 	context = {
 	"title": title,
 	}
 	return render(request, "home.html",context)
+
+def list_item(request):
+	title = 'List of Items'
+	queryset = Stock.objects.all()
+	context = {
+		"title": title,
+		"queryset": queryset,
+	}
+	return render(request, "list_item.html", context)

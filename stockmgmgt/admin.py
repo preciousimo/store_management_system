ae9ef0aaf07a7266e7756e98873aec5fff65fa5c
preciousimo <preciousimoniakemu@gmail.com>
2021-06-02 16:12:07 -0700

    Customize admin portal for better data view

--- stockmgmgt/admin.py
+++ stockmgmgt/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
+from .forms import StockCreateForm
 from .models import Stock
 
 # Register your models here.
-admin.site.register(Stock)
+
+class StockCreateAdmin(admin.ModelAdmin):
+   list_display = ['category', 'item_name', 'quantity']
+   form = StockCreateForm
+   list_filter = ['category']
+   search_fields = ['category', 'item_name']
+   
+   
+   
+admin.site.register(Stock, StockCreateAdmin)

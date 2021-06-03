a9707e53e15e77b762f8e64a5cb4d79c5d9583f1
preciousimo <preciousimoniakemu@gmail.com>
2021-06-03 11:49:20 -0700

    Add datefield(last updated, timestamp)

--- stockmgmgt/models.py
+++ stockmgmgt/models.py
@@ -28,6 +28,7 @@ class Stock(models.Model):
     created_by = models.CharField(max_length=50, blank=True, null=True)
     reorder_level = models.IntegerField(default='0', blank=True, null=True)
     last_updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 
     def __str__(self):
         return self.item_name + ' ' + str(self.quantity)

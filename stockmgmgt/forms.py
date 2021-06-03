c7243e595592f6fca40a8ad3ff2c2bdd763e20f4
preciousimo <preciousimoniakemu@gmail.com>
2021-06-03 01:20:16 -0700

    Validate and prevent duplicate data

--- stockmgmgt/forms.py
+++ stockmgmgt/forms.py
@@ -6,6 +6,25 @@ class StockCreateForm(forms.ModelForm):
     class Meta:
         model = Stock
         fields = ['category', 'item_name', 'quantity']
+        
+        # To prevent saving object with a blank item name
+        def clean_category(self):
+            category = self.cleaned_data.get('category')
+            if not category:
+                raise forms.ValidationError('This field is required')
+            for instance in Stock.objects.all():
+                
+                # Alerting and preventing the duplicate entry of computer name
+    			if instance.category == category:
+				raise forms.ValidationError(str(category) + ' is already created')
+ 
+            return category
+
+        def clean_item_name(self):
+            item_name = self.cleaned_data.get('item_name')
+            if not item_name:
+                raise forms.ValidationError('This field is required')
+            return item_name
 
 
 class StockSearchForm(forms.ModelForm):

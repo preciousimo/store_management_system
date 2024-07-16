dde0ec2455e6d63273319b91c7a1595cea87625d
preciousimo <preciousimoniakemu@gmail.com>
2024-07-16 11:10:27 +0100

    "Refactored Stock and StockHistory models into StockItem and StockAction models, added uniqueness constraints, and modified field types and defaults."

--- stockmgmgt/models.py
+++ stockmgmgt/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 category_choice = (
@@ -7,48 +8,44 @@ category_choice = (
     ('Phone', 'Phone'),
 )
 
-
 class Category(models.Model):
-    name = models.CharField(max_length=50, blank=True, null=True)
+    name = models.CharField(max_length=50, unique=True)  # Enforce uniqueness
 
     def __str__(self):
         return self.name
-    
-class Stock(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True)
-    item_name = models.CharField(max_length=50, blank=True, null=True)
-    quantity = models.IntegerField(default='0', blank=True, null=True)
-    receive_quantity = models.IntegerField(default='0', blank=True, null=True)
-    receive_by = models.CharField(max_length=50, blank=True, null=True)
-    supplier = models.CharField(max_length=50, blank=True, null=True)
-    issue_quantity = models.IntegerField(default='0', blank=True, null=True)
-    issue_by = models.CharField(max_length=50, blank=True, null=True)
-    issue_to = models.CharField(max_length=50, blank=True, null=True)
-    phone_number = models.CharField(max_length=50, blank=True, null=True)
+
+class StockItem(models.Model):  # Renamed to StockItem
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    item_name = models.CharField(max_length=50, unique=True) # Enforce uniqueness
+    quantity = models.IntegerField(default=0)  # Remove unnecessary default
+    reorder_level = models.IntegerField(default=0)
     created_by = models.CharField(max_length=50, blank=True, null=True)
-    reorder_level = models.IntegerField(default='0', blank=True, null=True)
-    last_updated = models.DateTimeField(auto_now_add=False, auto_now=True)
-    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
-    # date = models.DateTimeField(auto_now_add=False, auto_now=False)
+    last_updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.item_name + ' ' + str(self.quantity)
 
-class StockHistory(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
-    item_name = models.CharField(max_length=50, blank=True, null=True)
-    quantity = models.IntegerField(default='0', blank=True, null=True)
-    receive_quantity = models.IntegerField(default='0', blank=True, null=True)
-    receive_by = models.CharField(max_length=50, blank=True, null=True)
-    issue_quantity = models.IntegerField(default='0', blank=True, null=True)
-    issue_by = models.CharField(max_length=50, blank=True, null=True)
-    issue_to = models.CharField(max_length=50, blank=True, null=True)
+    def get_absolute_url(self):
+        return reverse('stock_detail', kwargs={'pk': self.pk})  # Add a get_absolute_url method
+
+
+class StockAction(models.Model):
+    item = models.ForeignKey(StockItem, on_delete=models.CASCADE)
+    action_type = models.CharField(max_length=10, choices=[
+        ('receive', 'Receive'),
+        ('issue', 'Issue')
+    ])
+    quantity = models.IntegerField()
     supplier = models.CharField(max_length=50, blank=True, null=True)
-    phone_number = models.CharField(max_length=50, blank=True, null=True)
-    created_by = models.CharField(max_length=50, blank=True, null=True)
-    reorder_level = models.IntegerField(default='0', blank=True, null=True)
-    last_updated = models.DateTimeField(auto_now_add=False, auto_now=False, null=True)
-    timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, null=True)
-    
-    def __str__(self):
-        return self.item_name + ' ' + str(self.quantity)
+    issue_to = models.CharField(max_length=50, blank=True, null=True)
+    performed_by = models.CharField(max_length=50)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.action_type == 'receive':
+            self.item.quantity += self.quantity
+        elif self.action_type == 'issue':
+            self.item.quantity -= self.quantity
+        self.item.save()
